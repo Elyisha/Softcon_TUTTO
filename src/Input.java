4742264cf818535082360899da9c0fc53e464b3f
Elyisha <116687078+Elyisha@users.noreply.github.com>
2022-11-18 14:22:41 +0100

    up

---
+++ src/Input.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Input {
+
+    public static String[] takeNames(){
+        String[] Names;
+        Scanner NameInput = new Scanner(System.in);
+
+        System.out.println("Put in your names seperated by a coma (example: Hans, Peter, Sara)");
+
+        while(true) {
+            String Input = NameInput.nextLine().replaceAll("\\s+", "");
+
+            if(Input.length() == 0) System.out.println("Must have at least 2 Players!");
+            else if(Input.matches(".*[0-9].*")) System.out.println("Names can't contain numbers!");
+            else {
+                return Names = Input.split(",");
+            }
+        }
+    }
+
+    public static Short takeMaxPoints(){
+        Short MaxPoints;
+        Scanner PointInput = new Scanner(System.in);
+
+        System.out.println("To how many points do you want to play?");
+
+        while(true) {
+            String Input = PointInput.nextLine().replaceAll("\\s+", "");
+
+            if(Input.length() == 0) System.out.println("Must type in an amount!");
+            else if(!Input.matches("[0-9]+")) System.out.println("Can only type-in numbers!");
+            else if(Integer.valueOf(Input) < 500 || Integer.valueOf(Input) > 32000)
+                System.out.println("Allowed point range is from 500 to 32000");
+            else return MaxPoints = Short.valueOf(Input);
+        }
+    }
+    public static char askUserDR(){
+        char DR;
+        Scanner DRInput = new Scanner(System.in);
+
+        System.out.println("Do you want to Display (D) or Roll (R)?");
+
+        while(true){
+            String Input = DRInput.nextLine().replaceAll("\\s","").toUpperCase();
+
+            if(Input.length() != 1) System.out.println("Must type in one character only!");
+            else if(!(Input.contains("R") || Input.contains("D"))) System.out.println("Must type in D or R");
+            else {
+                DR = Input.charAt(0);
+                return DR;
+            }
+        }
+    }
+    public static char askUserRE(){
+        char RE;
+        Scanner REInput = new Scanner(System.in);
+
+        System.out.println("Do you want to Roll (R) or End (E)?");
+
+        while(true){
+            String Input = REInput.nextLine().replaceAll("\\s","").toUpperCase();
+
+            if(Input.length() != 1) System.out.println("Must type in one character only!");
+            else if(!(Input.contains("R") || Input.contains("E"))) System.out.println("Must type in R or E");
+            else {
+                RE = Input.charAt(0);
+                return RE;
+            }
+        }
+    }
+    /*
+    public static Dice[] {
+
+
+
+
+
+    }
+    */
+}
