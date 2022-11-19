4bb9d63b5c8160d14db4f61795988ed214b33b6b
samuelfrnk <samuelandreas.frank@uzh.ch>
2022-11-19 17:34:34 +0100

    Player & Display Class

---
+++ src/Display.java
@@ -0,0 +1,50 @@
+import Cards.*;
+import Dices.*;
+
+public class Display {
+
+    public static void displayDice(diceNumber d1) {
+        printUpper();
+        for(int i = 1; i<4; i++){
+            printLeftBox();
+            printInner(d1, i);
+            printRightBox();
+        }
+        printUnder();
+    }
+
+    public static void displayCard(CardsValue c1){
+        System.out.println("You drew following card: " + c1);
+    }
+
+    public static void displayPoints(Player P1){
+        System.out.println("You've got " + P1.getPoints() + " Points.");
+    }
+
+    private static void printInner(diceNumber d1, int i){
+        if(d1 == diceNumber.SIX) System.out.print(" o   o ");
+        else if(d1 == diceNumber.ONE && i == 2) System.out.print("   o   ");
+        else if(d1 == diceNumber.TWO && i == 1) System.out.print("     o ");
+        else if(d1 == diceNumber.TWO && i == 3) System.out.print(" o     ");
+        else if(d1 == diceNumber.THREE && i == 1) System.out.print("     o ");
+        else if(d1 == diceNumber.THREE && i == 2) System.out.print("   o   ");
+        else if(d1 == diceNumber.THREE && i == 3) System.out.print(" o     ");
+        else if(d1 == diceNumber.FOUR && (i == 3||i==1)) System.out.print(" o   o ");
+        else if(d1 == diceNumber.FIVE && (i == 3||i==1)) System.out.print(" o   o ");
+        else if(d1 == diceNumber.FIVE && i == 2) System.out.print("   o   ");
+        else System.out.print("       ");
+    }
+
+    private static void printUpper(){
+        System.out.println(" _________");
+    }
+    private static void printLeftBox(){
+        System.out.print("| ");
+    }
+    private static void printRightBox(){
+        System.out.println(" |");
+    }
+    private static void printUnder(){
+        System.out.println("|_________|");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Cards.CardsValue;
 import Dices.*;
 public class Main {
 
