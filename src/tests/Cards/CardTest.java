143a866fa64a0ae91b422b1da26c568f4e77d2b6
samuelfrnk <samuelandreas.frank@uzh.ch>
2022-11-26 17:33:40 +0100

    tets

--- src/tests/Cards/CardTest.java
+++ src/tests/Cards/CardTest.java
@@ -1,11 +1,13 @@
 package Cards;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-
+    @Test
+    void dummyTest(){
+        assertEquals(1,1);
+    }
 
 }
