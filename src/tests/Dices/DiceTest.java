b2b021dc180470e7afc069ed00d0393a2f9d9163
samuelfrnk <samuelandreas.frank@uzh.ch>
2022-11-26 17:45:46 +0100

    test

---
+++ src/tests/Dices/DiceTest.java
@@ -0,0 +1,7 @@
+package Dices;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+}
---
+++ src/tests/Dices/ValidDiceTest.java
@@ -0,0 +1,10 @@
+package Dices;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidDiceTest {
+
+
+}
