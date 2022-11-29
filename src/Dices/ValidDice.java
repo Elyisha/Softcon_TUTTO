567f50737795126928fc8c0f64d51aaccee8da6d
Elyisha <116687078+Elyisha@users.noreply.github.com>
2022-11-29 14:06:57 +0100

    up

--- src/Dices/ValidDice.java
+++ src/Dices/ValidDice.java
@@ -7,11 +7,16 @@ import java.util.List;
 
 public class ValidDice {
 
-
+    //Checks if there are any dices in the dice array that do not give any points and are
+    //therefore invalid
     public static boolean isValidGuess(Dice[] dices) {
+
+        //converts dice-array to a list to get a value-array back with the values of every dice
+        //e.g. if there are 3 dices with value 5 then the value array at index 4 will contain 3
         List<Dice> list = Arrays.asList(dices);
         int[] values = getValueArray(list, false);
 
+        //for-loop does the checking
         for(byte i = 1; i < values.length;++i) {
             if(i == 4) continue;
             if(values[i] > 0 && values[i] != 3) {
@@ -19,9 +24,9 @@ public class ValidDice {
             }
         }
         return true;
-
     }
 
+    //Checks whether there is a die put Aside that has the same value with the one being put Aside (only in Straight)
     public static boolean hasNoDuplicates(Dice[] dices, diceNumber diceNum) {
         for (Dice dice : dices) {
             if (dice.isAside() && dice.getDiceNumber() == diceNum) return false;
@@ -29,50 +34,66 @@ public class ValidDice {
         return true;
     }
 
+    //checks whether there is a valid dice left in the dice-array that can be put-Aside (only in Straight)
     public static boolean hasValidDicesLeft(ArrayList<Dice> dices){
+
+        //gets value array with the values of the dices
         int[] values = getValueArray(dices, true);
 
+        //does the checking
         for (Dice dice : dices) {
             if (!dice.isAside()) {
                 if (values[dice.getDiceNumber().ordinal()] == 0) return true;
             }
         }
-
         return false;
     }
 
+    //returns an array with the values of the dices in the dice-list, when use for straight there
+    //is an additional condition
     private static int[] getValueArray(List<Dice> dices, boolean straight) {
         int[] values = new int[6];
         Arrays.fill(values, 0);
 
+        //sorts out the values and puts them into the value array
         for (Dice dice : dices) {
             if(!straight)
                 values[dice.getDiceNumber().ordinal()] += 1;
             else if(dice.isAside()) values[dice.getDiceNumber().ordinal()] += 1;
         }
-
         return values;
     }
 
+    //counts the points the dices in the ArrayList give according to the game-rules
     public static short countPoints(ArrayList<Dice> dices) {
-        short points = 0;
-        int triplet = 0;
+
+        //gets value-array and declares helper variables
         int[] values = getValueArray(dices, false);
+        short points = 0;
+        int triplet;
         boolean hasTriplet = true;
 
-
-
+        //does the counting, first checks the value-array for triplets, if none have been found it will look
+        //for single die that give points (fives and ones)
         while(true) {
             if (hasTriplet) {
+
+                //checks for triplets
                 triplet = findTriplet(values);
+
+                //adds points if there have been found any triplets, else sets hasTriplet to false
                 if(triplet == 0) hasTriplet = false;
-                else if(triplet == 1) {points += 1000; triplet = 0;}
-                else {points += triplet*100; triplet = 0;}
+                else if(triplet == 1) points += 1000;
+                else points += triplet*100;
             } else {
+
+                //checks for ones
                 if(values[0] > 0) {
                     points += 100;
                     values[0] -= 1;
                 }
+
+                //checks for fives
                 else if (values[4] > 0) {
                         points += 50;
                         values[4] -= 1;
@@ -80,10 +101,10 @@ public class ValidDice {
                 else break;
             }
         }
-
         return points;
     }
 
+    //finds a triplet in a value array
     private static int findTriplet(int[] values) {
         for (byte i = 0; i < values.length; ++i) {
             if (values[i] >= 3) {
--- src/Input/DecideDice.java
+++ src/Input/DecideDice.java
@@ -7,45 +7,62 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DecideDice {
+
+    //asks the user which dices he wants to put aside and validates the user's choice
     public static short decideDice(Dice[] dices, boolean fireworks) {
+
+        //Initializes scanner and prints starting message
         Scanner DDInput = new Scanner(System.in);
         System.out.println("Which dices do you want to put aside? Enter your decision by separating " +
                 "the dice-index with a comma (e.g. 2,4,5).");
 
-
+        //takes and validates input, while loop will end as soon as the user gives a valid input
         while(true) {
+
+            //creates list with the indices of the dice that the user wants to put aside and converts the
+            //string input to char-array
             List<Integer> numbers;
             String sInput = DDInput.nextLine().replaceAll("\\s","");
             char[] Input = sInput.toCharArray();
 
+            //Validates the users choice of dices
             numbers = InputValidation(Input, dices, false);
             if(numbers == null) continue;
 
-            //Check whether there are single dices that don't give any points
+            //creates dice-array with the user's choice of dices
             Dice[] countdices = new Dice[numbers.size()];
             for (int i = 0; i < numbers.size();++i) countdices[i] = dices[numbers.get(i)-1];
 
+            //Check whether there are single dices that don't give any points (additional input-validation)
             if(!ValidDice.isValidGuess(countdices)) {
                 System.out.println("Must only take out dices that give points");
                 continue;
             }
 
-            //Count points and return
+            //Counts the points of the dices the user wants to put aside and returns it
+            //if the current card is fireworks, then the dices won't actually be put aside
+            //but their points will be counted
             ArrayList<Dice> countDices = new ArrayList<>(numbers.size());
-
             for (byte i = 0; i < numbers.size(); i++) {
                 if(!fireworks) dices[numbers.get(i)-1].putAside();
                 countDices.add(i, dices[numbers.get(i) - 1]); //add to countDices
             }
+
+            //prints achieved points and returns them
             if (!fireworks) System.out.println("You put " + Dices.ValidDice.countPoints(countDices) + " aside.");
             return Dices.ValidDice.countPoints(countDices);
         }
     }
+
+    //asks the user which dices he wants to put aside if the card is "straight" and validates the user's choice
     public static void straightDecideDice(Dice[] dices) {
+
+        //Initializes scanner and prints starting message
         Scanner sDDInput = new Scanner(System.in);
         System.out.println("Which dices do you want to put aside? Enter your decision by separating " +
                 "the dice-index with a comma (e.g. 2,4,5).");
 
+        
         while(true) {
             List<Integer> numbers;
             String sInput = sDDInput.nextLine().replaceAll("\\s","");
@@ -117,5 +134,4 @@ public class DecideDice {
     private static boolean alreadyAside(Dice[] dices, int i) {
         return dices[i].isAside();
     }
-
 }
